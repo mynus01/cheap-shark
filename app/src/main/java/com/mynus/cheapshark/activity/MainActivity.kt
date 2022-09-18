0274b1929e9ce86e05cedafc993e36e668c81e94
Matheus Sandri <matheusgsandri@gmail.com>
2022-09-18 19:41:40 -0300

    refactor: resolve merge conflicts

--- app/src/main/java/com/mynus/cheapshark/activity/MainActivity.kt
+++ app/src/main/java/com/mynus/cheapshark/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.mynus.cheapshark
+package com.mynus.cheapshark.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/main/java/com/mynus/cheapshark/application/BaseApplication.kt
+++ app/src/main/java/com/mynus/cheapshark/application/BaseApplication.kt
@@ -1,4 +1,4 @@
-package com.mynus.cheapshark
+package com.mynus.cheapshark.application
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
