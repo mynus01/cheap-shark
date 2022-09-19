eaaf3271e0ce67b065ed59a8aebbde361aed33e3
Matheus Sandri <matheusgsandri@gmail.com>
2022-09-19 09:21:56 -0300

    refactor: recreate main and details screen

--- domain/src/main/java/com/mynus/domain/util/Constants.kt
+++ domain/src/main/java/com/mynus/domain/util/Constants.kt
@@ -4,4 +4,8 @@ object Constants {
     object Endpoints {
         const val DEALS = "deals?storeID=1"
     }
+    object Urls {
+        const val STEAM_APP = "https://store.steampowered.com/app/"
+        const val METACRITIC = "https://www.metacritic.com"
+    }
 }
--- presentation/src/main/java/com/mynus/presentation/fragment/DetailsBottomSheetFragment.kt
+++ presentation/src/main/java/com/mynus/presentation/fragment/DetailsBottomSheetFragment.kt
@@ -1,11 +1,15 @@
 package com.mynus.presentation.fragment
 
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isGone
+import androidx.core.view.isInvisible
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.mynus.domain.model.Deal
+import com.mynus.presentation.R
 import com.mynus.presentation.databinding.FragmentDetailsBinding
 import com.mynus.presentation.util.loadImage
 
@@ -28,6 +32,16 @@ class DetailsBottomSheetFragment(
         binding.apply {
             tvTitle.text = deal.title
             ivThumb.loadImage(deal.thumb)
+            tvNormalPrice.text = root.context.getString(R.string.deal_price_template, deal.normalPrice)
+            if (deal.isOnSale && deal.salePrice.isNotBlank()) {
+                tvSalePrice.text = root.context.getString(R.string.deal_price_template, deal.salePrice)
+                tvSalePrice.isInvisible = false
+                tvNormalPrice.paintFlags = tvNormalPrice.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            } else {
+                tvSalePrice.isInvisible = true
+                tvNormalPrice.paintFlags = tvNormalPrice.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+                tvNormalPrice.paintFlags = tvNormalPrice.paintFlags
+            }
         }
     }
 }
